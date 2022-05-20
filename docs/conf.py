3a9fceb0316640b5fc988eebf195610919710ed6
Marius Bakke <marius.bakke@usit.uio.no>
2022-05-20 17:39:49 +0200

    Publish documentation to GitHub Pages.

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 # Add the parent directory on PYTHONPATH.
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
