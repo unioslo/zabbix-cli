1bb7a34b54567195bc9d9439e0a011f1e6ca15ae
Retyunskikh Dmitriy <retyunskikh@alkor.co.ru>
2023-03-07 18:07:43 +0300

    test

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ try:
     data_files = None
     current_os = platform.platform()
     if "Linux" in current_os:
-        data_files = [('/usr/share/zabbix-cli', ['etc/zabbix-cli.conf'])]
+        data_files = [('usr/share/zabbix-cli', ['etc/zabbix-cli.conf'])]
     elif "Darwin" in current_os:
         data_files = [('/usr/local/bin/zabbixcli', ['etc/zabbix-cli.conf'])]
 
