b715d650f49bfa816ca40989e4c6824d0fbabf64
pederhan <pederhan@usit.uio.no>
2024-08-08 15:18:43 +0200

    Add pluralize_no_count
    Maybe this is dogshit???

--- zabbix_cli/output/formatting/grammar.py
+++ zabbix_cli/output/formatting/grammar.py
@@ -13,19 +13,39 @@ def pluralize(word: str, count: int, with_count: bool = True) -> str:
     """Pluralize a word based on a count.
 
     Examples:
-    >>> pluralize("apple", 1)
+    >>> from zabbix_cli.output.formatting.grammar import pluralize as p
+    >>> p("apple", 1)
     '1 apple'
-    >>> pluralize("apple", 2)
+    >>> p("apple", 2)
     '2 apples'
-    >>> pluralize("category", 1)
+    >>> p("category", 1)
     '1 category'
-    >>> pluralize("category", 2)
+    >>> p("category", 2)
     '2 categories'
-    >>> pluralize("category", 0)
+    >>> p("category", 0)
     '0 categories'
-    >>> pluralize("category", 0, with_count=False)
+    >>> p("category", 0, with_count=False) # see pluralize_no_count
     'categories'
     """
     if with_count:
         return f"{count} {_pluralize_word(word, count)}"
+    return _pluralize_word(word, count)
+
+
+def pluralize_no_count(word: str, count: int) -> str:
+    """Pluralize a word without a count prepended to the pluralized word.
+
+    Shortcut for `pluralize(word, count, with_count=False)`.
+
+    Examples:
+    >>> from zabbix_cli.output.formatting.grammar import pluralize_no_count as pnc
+    >>> pnc("apple", 1)
+    'apple'
+    >>> pnc("apple", 2)
+    'apples'
+    >>> pnc("category", 1)
+    'category'
+    >>> pnc("category", 2)
+    'categories'
+    """
     return _pluralize_word(word, count)
