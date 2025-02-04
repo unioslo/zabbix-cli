6e66bbfe953b144c8e949a06903ebb25b44cafe3
Peder Hovdan Andresen <107681714+pederhan@users.noreply.github.com>
2025-02-04 11:45:44 +0100

    feat: Add `create_notification_user --dryrun` (#294)

--- zabbix_cli/commands/results/user.py
+++ zabbix_cli/commands/results/user.py
@@ -4,3 +4,38 @@
 # most of the result rendering was implemented in the Pyzabbix models
 # themselves instead of as result classes. That is why this module is
 # (mostly) empty.
+from __future__ import annotations
+
+from zabbix_cli.models import AggregateResult
+from zabbix_cli.models import ColsRowsType
+from zabbix_cli.models import ColsType
+from zabbix_cli.models import RowsType
+from zabbix_cli.models import TableRenderable
+from zabbix_cli.pyzabbix.types import Usergroup
+from zabbix_cli.pyzabbix.types import UserMedia
+
+
+class CreateNotificationUserResult(TableRenderable):
+    """Result type for creating a notification user."""
+
+    username: str
+    userid: str
+    media: list[UserMedia]
+    usergroups: list[Usergroup]
+
+    def __cols_rows__(self) -> ColsRowsType:
+        cols: ColsType = [
+            "User ID",
+            "Username",
+            "Media",
+            "Usergroups",
+        ]
+        rows: RowsType = [
+            [
+                self.userid,
+                self.username,
+                AggregateResult(result=self.media).as_table(),
+                "\n".join([ug.name for ug in self.usergroups]),
+            ]
+        ]
+        return cols, rows
